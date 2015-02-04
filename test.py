cc5e2ef83ef399c31b60018a7fc6f9041d8a40ba
Hyunsoo Park <rex8312@gmail.com>
2015-02-04 16:59:48 +0900

    BaseEstimator import

--- test.py
+++ test.py
@@ -2,8 +2,14 @@
 
 __author__ = 'rex8312'
 
+from pyELM.pyELM import ExtreamLearningMachine
+
 
 if __name__ == '__main__':
-    print 'pyELM'
+    clf = ExtreamLearningMachine()
+
+    X, y = ['foo', 'bar', 'foo'], [1, 0, 1]
+    clf.fit(X, y)
+    print clf.predict(X)
 
 
