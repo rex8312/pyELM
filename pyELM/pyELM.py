ed186b9f51989db44462b47e5c3ea5c13581790b
Hyunsoo Park <rex8312@gmail.com>
2015-02-04 19:46:32 +0900

    basic ELM 구현

---
+++ pyELM/pyELM.py
@@ -0,0 +1,37 @@
+__author__ = 'rex8312'
+
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.linear_model import Perceptron
+
+
+class BasicExtreamLearningMachine(BaseEstimator, ClassifierMixin):
+
+    def __init__(self):
+        self.n = 8
+        self.a = None
+        self.b = None
+        self.clf = Perceptron()
+
+    def g_func(self, X):
+        X1 = 1. / (1. + np.exp(-X))
+        return X1
+
+    def fit(self, X, y):
+        self.a = np.random.random((self.n, X.shape[1]))
+        H = self.g_func(X.dot(self.a.T))
+        inv_H = np.matrix(H).getI()
+        self.b = inv_H.dot(y)
+        self.clf.fit(X, y)
+        return self
+
+    def predict(self, X):
+        H = self.g_func(X.dot(self.a.T))
+        y_0 = self.g_func(H.dot(self.b.T))
+        y_1 = list()
+        for y in y_0:
+            if y > 0.5:
+                y_1.append(1.0)
+            else:
+                y_1.append(0.0)
+        return y_1
--- test.py
+++ test.py
@@ -2,14 +2,31 @@
 
 __author__ = 'rex8312'
 
-from pyELM.pyELM import ExtreamLearningMachine
+from sklearn.datasets import load_iris
+from sklearn.preprocessing import normalize
+from sklearn import cross_validation
 
+from pyELM.pyELM import BasicExtreamLearningMachine
+
+
+def func(idx):
+    def _func(x):
+        if x == idx:
+            return 1.0
+        else:
+            return 0.0
+    return _func
 
 if __name__ == '__main__':
-    clf = ExtreamLearningMachine()
+    data = load_iris()
+    X = normalize(data.data, axis=0)
+    y0 = map(func(0), data.target)
+    y1 = map(func(1), data.target)
+    y2 = map(func(2), data.target)
 
-    X, y = ['foo', 'bar', 'foo'], [1, 0, 1]
-    clf.fit(X, y)
-    print clf.predict(X)
 
+    clf = BasicExtreamLearningMachine()
+    for y in [y0, y1, y2]:
+        scores = cross_validation.cross_val_score(clf, X, y, cv=10)
+        print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
 
